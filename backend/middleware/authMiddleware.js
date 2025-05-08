bcc55da2fcce37e8dc6aa4a37a0120b9e6bf028d
Dmitriy <dimasikemelka@gmail.com>
2025-05-08 14:47:49 +0300

    added CRUD for our-work

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,14 +1,13 @@
 const jwt = require('jsonwebtoken');
 
-
+const secretKey = require("../env")
 
 function verifyToken(req, res, next) {
     const token = req.header('Authorization');
-    console.log(token)
 
     if (!token) return res.status(401).json({ error: 'Access denied' });
     try {
-        const decoded = jwt.verify(token, 'alo');
+        const decoded = jwt.verify(token, secretKey);
         req.userId = decoded.userId;
         next();
     } catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
-const mongodb = require('./env')
+const { mongodb , PORT } = require('./env')
 const express = require('express');
+const ourWorkRouter = require('./routes/ourWork')
+
 const app = express();
+
 app.use(express.json());
+
+app.use('/our-work', ourWorkRouter)
 
 mongoose.connect(mongodb, {
     useNewUrlParser: true,
@@ -10,4 +15,7 @@ mongoose.connect(mongodb, {
 }).then(() => console.log("MongoDB connected"))
   .catch(err => console.error("Connection error:", err));
 
-  
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

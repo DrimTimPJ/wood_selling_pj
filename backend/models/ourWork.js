a996af341fa543e191b66d5ac476c794db959ed5
Topchagg <zahar.meednov@gmail.com>
2025-05-11 10:06:19 +0200

    Removed extra files

--- backend/models/ourWork.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const ourWorkSchema = new mongoose.Schema({
-    image: { type: String, required: true },
-    price: { type: Number, required: true }
-});
-module.exports = mongoose.model('ourWork', ourWorkSchema);

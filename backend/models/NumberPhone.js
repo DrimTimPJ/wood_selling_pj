08bf216b8727e845d8ddf5d26946f161424b23d4
Topchagg <zahar.meednov@gmail.com>
2025-05-11 09:04:26 +0200

    removed extra files

--- backend/models/NumberPhone.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const NumberPhoneSchema = new mongoose.Schema({
-    data: { type: String, unique: true, required: true },
-});
-module.exports = mongoose.model('NumberPhone', NumberPhoneSchema);
--- backend/models/UserQuestion.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const UserQuestionSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    telephoneNumber: { type: String, required: true },
-    question: { type: String, required: true }
-});
-module.exports = mongoose.model('UserQuestion', UserQuestionSchema);
